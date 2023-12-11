9d719f850b514fbf9ee6e01e59fadbefbb7bb967
Fisher Aderinola <adeinola2005@gmail.com>
2023-12-11 12:32:48 +0100

    My first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () =>{
+   navLinks.style.display = "block";
+   navLinks.style.textAlign = "center";
+})
+
+ 
+
